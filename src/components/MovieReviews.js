bcb59e90147a34f682b49316e172ec9c42e51121
Blaire Baker <bakerblaire@gmail.com>
2021-02-04 18:22:14 -0600

    done

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,11 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = (props) => {
+    return(
+        <ul className="review-list">
+            {props.reviews.map(review => <li className="review">{review.byline}</li>)}
+        </ul>
+    )
+} 
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -5,5 +5,47 @@ import MovieReviews from './MovieReviews'
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
+                        
 
-// Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component{
+    constructor() {
+        super()
+        this.state = {
+            reviews: [],
+            searchTerm: ""
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            searchTerm: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        fetch(URL + `&query=${this.state.searchTerm}`)
+        .then(resp => resp.json())
+        .then(data => {
+            this.setState({
+                reviews: data.results
+            })
+        })
+    }
+
+    render(){
+        return(
+            <div className="searchable-movie-reviews">
+                <form onSubmit={event => this.handleSubmit(event)}>
+                    <label>Search for Reviews:</label>
+                    <input type="text"
+                        onChange={event => this.handleChange(event)}></input>
+                    <input type="submit"></input>
+                </form>
+                {this.state.reviews ? <MovieReviews reviews={this.state.reviews}/> : null }
+            </div>
+        )
+    }
+}
+
+export default SearchableMovieReviewsContainer;
